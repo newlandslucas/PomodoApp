08dab9afa424df0014da95bd46e4f6f58fe1c0f8
C1326386 Lucas Silveira Newlands Machado <lucas.newlands@ibm.com>
2022-08-17 21:25:41 -0300

    Project setup

---
+++ PomodoroApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PomodoroApp
+//
+//  Created by Lucas Newlands on 17/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Lucas Newlands on 17/08/22.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
